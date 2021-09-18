960e05a772051cf65df2f0af417fb0f76c863092
Javier <framirez76@uabc.edu.mx>
2021-09-17 22:49:31 -0700

    Add player 1 and player 2 buttons
    Add buttons and p tag that displays player time.
    Add buttons event listener
    Refactor startTimer() function to be reusable by other buttons
    Format some code

--- main.js
+++ main.js
@@ -1,30 +1,47 @@
 let hours = 0, minutes = 0, seconds = 0;
+//Version 1
 const timer1 = document.querySelector("#timer1");
 const startButton = document.querySelector("#start");
 const resetButton = document.querySelector("#reset");
+//Version 2 player 1
+const timerPlayer1 = document.querySelector("#timerPlayer1");
+const startButtonPlayer1 = document.querySelector("#startPlayer1");
+const resetButtonPlayer1 = document.querySelector("#resetPlayer1");
+//Version 2 player 2
+const timerPlayer2 = document.querySelector("#timerPlayer2");
+const startButtonPlayer2 = document.querySelector("#startPlayer2");
+const resetButtonPlayer2 = document.querySelector("#resetPlayer2");
+
 let interval;
 eventListeners();
 
 function eventListeners() {
+    //Version 1 timer
     startButton.addEventListener("click", startTimer);
     resetButton.addEventListener("click", resetTimer);
+    //Version 2 timer
+    startButtonPlayer1.addEventListener("click", e => { });
+    resetButtonPlayer1.addEventListener("click", e => { });
+    startButtonPlayer2.addEventListener("click", e => { });
+    resetButtonPlayer2.addEventListener("click", e => { });
+
 }
 
-function startTimer() {
-    
-    if (startButton.textContent == "Start") {
+function startTimer(e) {
+    const button = e.srcElement;
+    if (button.textContent == "Start") {
         interval = setInterval(timer, 1000);
-        startButton.textContent = "Stop";
-    }else{
+        button.textContent = "Stop";
+    } else {
         clearInterval(interval);
-        startButton.textContent = "Start";
+        button.textContent = "Start";
     }
 }
 
-function resetTimer(){
-    hours=0;
-    minutes=0;
-    seconds=0;
+function resetTimer() {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
     timer1.textContent = "00:00:00";
 }
 
